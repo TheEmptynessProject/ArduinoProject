d654a5452f3385d610085159ae1e2342a93ca8b1
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2022-12-07 12:44:44 +0000

    Update project.ino

--- project.ino
+++ project.ino
@@ -10,23 +10,20 @@ String array[] = {
 };
 
 
-MFRC522 mfrc522(10,8);   // Create MFRC522 instance.
+MFRC522 mfrc522(10,8);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
   pinMode(5, OUTPUT); // red led
   pinMode(6, OUTPUT); //green led
-  Serial.begin(9600); // Initialize serial communications with the PC
-  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-  SPI.begin();        // Init SPI bus
+  Serial.begin(9600);
+  while (!Serial);
+  SPI.begin();
   mfrc522.PCD_Init();
 Wire.begin();
   lcd.begin();
 }
 
-/**
- * Main loop.
- */
 void loop() {
 String xxx;
       Serial.print("ADMIN\n");
@@ -41,7 +38,6 @@ if ( ! mfrc522.PICC_IsNewCardPresent())
   {
     return;
   }
-  // Select one of the cards
   if ( ! mfrc522.PICC_ReadCardSerial()) 
   {
     return;
@@ -87,7 +83,6 @@ if (xxx == array[i]){
   }
       break;
     }
-      
   
 }
 
